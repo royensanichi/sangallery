a8923d9814a685ff5f7fd3c1cb933a2841877d8c
Sany <sanichitrix@gmail.com>
2025-06-12 13:33:50 +0700

    quick update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,8 @@ export default function Footer() {
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
 
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <label href="https://flowbite.com/" className="hover:underline">SanGallery by SanichiDev™</label>. All Rights Reserved.</span>
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <label href="https://flowbite.com/" className="hover:underline">SanGallery by SanichiDev™</label>. All Rights Reserved.</span>
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <label href="https://flowbite.com/" className="hover:underline">Developed with Flowbite</label></span>
             </div>
         </footer>
     )
